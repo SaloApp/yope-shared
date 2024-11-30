3430fdd4962ce1b116a634ca5e26396ceeca5678
Pavel Rudkouski <pavel@salo.team>
2024-11-30 22:05:41 +0000

    add Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Shared",
-            url: "https://github.com/SaloApp/yope-shared/releases/download/v0.1/Shared.xcframework.tar.gz.zip",
+            url: "https://github.com/SaloApp/yope-shared/releases/download/v0.1.1/Shared.xcframework.tar.gz.zip",
             checksum: "45f9c32ef46671de0e15e61c07ab70b0cf1cc6f946af533d82f65df7b2660bb3"
         )
     ]
