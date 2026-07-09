ebfce7b2e8302970c35cb1032dd125802b53b7e5
GitHub Action <action@github.com>
2026-07-09 11:57:11 -0400

    Update SharedData framework (build 20260709_115639)

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Shared",
-            url: "https://storage.googleapis.com/saloapp-ios-frameworks/frameworks/ios-main-stable/20260708_121408/SharedData.xcframework.zip",
-            checksum: "b09c6aae90ad571b656593a9aac0cab0a2e564248ea665ece7912abd44e0b909"
+            url: "https://storage.googleapis.com/saloapp-ios-frameworks/frameworks/ios-main-stable/20260709_115639/SharedData.xcframework.zip",
+            checksum: "be7a68ac1197c5f3e49b2bdf5e1c6d5202b9108dd7fc68a6e658b74cd554c02a"
         )
     ]
 )
